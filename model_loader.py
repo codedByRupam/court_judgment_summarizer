bb8ae70573a0fb52d5bfd97f1e3630a50cd7df9a
Rupam Kumari <ritukumari26102004@gmail.com>
2026-06-03 11:53:54 +0530

    Deployment fixes

--- model_loader.py
+++ model_loader.py
@@ -1,14 +1,14 @@
 import streamlit as st
+import transformers
 from transformers import pipeline
 
+print("Transformers version:", transformers.__version__)
 
 @st.cache_resource
 def load_model():
-
     return pipeline(
-        "summarization",
+        task="summarization",
         model="sshleifer/distilbart-cnn-12-6"
     )
-
 
 summarizer_model = load_model()
