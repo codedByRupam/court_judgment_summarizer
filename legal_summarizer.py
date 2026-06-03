08cb408b244177c359d5fd3e477759c632cd1dd9
Rupam Kumari <ritukumari26102004@gmail.com>
2026-06-03 12:02:24 +0530

    Fix model name typo

--- legal_summarizer.py
+++ legal_summarizer.py
@@ -2,9 +2,8 @@ from transformers import pipeline
 
 summarizer = pipeline(
     "summarization",
-    model="sshleifer/distilbart-cnn-12-6n"
+    model="sshleifer/distilbart-cnn-12-6"
 )
-
 
 def summarize_section(text):
 
